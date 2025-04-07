36178026f8cd8751f01978c0c8189420b26c8e87
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-04-06 20:44:46 -0500

    Concectando con endpoints de buscar por id, actualizar y eliminar

--- RoomiesU_frontend/scripts/usuario.js
+++ RoomiesU_frontend/scripts/usuario.js
@@ -21,7 +21,7 @@ function loadAdmins(){
                     <td>${element.descripcion}</td>
                     <td>
                         <button type="button" class="btn btn-primary" onclick='window. location =
-                        "form_proveedores.html?idproveedor=${element.id}"'>Ver</button>
+                        "form_usuario.html?idusuario=${element.id}"'>Ver</button>
                     </td>
                 </tr>
             ` 
@@ -58,8 +58,8 @@ function loadEstudiantes(){
                     <td>${element.edad}</td>
                     <td>${element.descripcion}</td>
                     <td>
-                        <button type="button" class="btn btn-primary" onclick='window. location =
-                        "form_proveedores.html?idproveedor=${element.id}"'>Ver</button>
+                        <button type="button" class="btn btn-primary" onclick='window.location =
+                        "form_usuario.html?idusuario=${element.id}"'>Ver</button>
                     </td>
                 </tr>
             ` 
@@ -71,5 +71,77 @@ function loadEstudiantes(){
                 <td colspan="5">Error al recuperar los datos. </td>
             </tr>
         `;
+    }
+}
+
+//Función para cargar los datos del usuario en el formulario de edición
+function loadUsuario(idusuario){
+    let request = sendRequest('api/user/list/'+idusuario, 'GET', '')
+    let id = document.getElementById('usuario_id')
+    let rol = document.getElementById('rol')
+    let username = document.getElementById('username')
+    let nombre = document.getElementById('nombre')
+    let apellido = document.getElementById('apellido')
+    let identificacion = document.getElementById('identificacion')
+    let email = document.getElementById('email')
+    let telefono = document.getElementById('telefono')
+    let edad = document.getElementById('edad')
+    let descripcion = document.getElementById('descripcion')
+    request.onload = function(){
+        let data = request.response;
+        //Se actualiza el valor de las variables según el JSON
+        console.log(data);
+        id.value = data.id
+        rol.value = data.roles.map(r => r.name).join(", ")
+        username.value = data.username
+        nombre.value = data.nombre
+        apellido.value = data.apellido
+        identificacion.value = data.identificacion 
+        email.value = data.email
+        telefono.value = data.telefono
+        edad.value = data.edad
+        descripcion.value = data.descripcion
+    }
+    request.onerror = function(){
+        alert("Error al recuperar los datos.");
+    }
+}
+
+//Función para editar los datos del usuario en el formulario de edición
+function saveUsuario(){
+    let id = document.getElementById('usuario_id').value
+    let rol = document.getElementById('rol').value
+    let username = document.getElementById('username').value
+    let nombre = document.getElementById('nombre').value
+    let apellido = document.getElementById('apellido').value
+    let identificacion = document.getElementById('identificacion').value
+    let email = document.getElementById('email').value
+    let telefono = document.getElementById('telefono').value
+    let edad = document.getElementById('edad').value
+    let descripcion = document.getElementById('descripcion').value
+    let data = {'id': id,'rol':rol,'username':username,'nombre': nombre, 
+        'apellido': apellido, 'identificacion': identificacion, 'email': email,
+        'telefono': telefono, 'edad': edad, 'descripcion': descripcion}
+    console.log(data);
+    let request = sendRequest('api/user/update', id ? 'PUT' : 'POST', data)
+    request.onload = function(){
+        alert('Usuario Creado o actualizado Exitosamente.')
+        window.location = 'home.html';
+    }
+    request.onerror = function(){
+        alert('Error al guardar los cambios.')
+    }
+}
+
+//Función para eliminar el usuario en el formulario de edición
+function deleteUsuario(){
+    let id = document.getElementById('usuario_id').value
+    let request = sendRequest('api/user/delete/'+ id , 'DELETE', '')
+    request.onload = function(){
+        alert('Registro Eliminado Exitosamente.')
+        window.location = 'home.html';
+    }
+    request.onerror = function(){
+        alert('Error al guardar los cambios.')
     }
 }
